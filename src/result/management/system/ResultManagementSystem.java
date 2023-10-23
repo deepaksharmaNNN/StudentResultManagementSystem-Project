106063901260d567e0dbef13486c59712ad35ccb
Deepak Sharma <dsharma2828@gmail.com>
2023-10-23 22:48:02 +0530

    Initial commit

---
+++ src/result/management/system/ResultManagementSystem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package result.management.system;
+
+/**
+ *
+ * @author Deepak Sharma
+ */
+public class ResultManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         // Create an instance of the index class
+        index mainWindow = new index();
+        
+        // Set the visibility of the main window
+        mainWindow.setVisible(true);
+    }
+    
+}
